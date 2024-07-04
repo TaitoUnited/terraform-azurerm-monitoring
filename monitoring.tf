b11bf1629f7b5f02505df9f086d4b4e0bdbf483d
keskiju <jukka.keskiluopa@taitounited.fi>
2024-07-04 13:53:02 +0300

    feat: set azurerm_application_insights.workspace_id

--- monitoring.tf
+++ monitoring.tf
@@ -19,6 +19,7 @@ resource "azurerm_application_insights" "insights" {
   location            = var.location
   resource_group_name = var.resource_group_name
   application_type    = "web"
+  workspace_id        = azurerm_log_analytics_workspace.logging.id
 }
 
 /* TODO: slack webhook support */
